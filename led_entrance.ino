4bbb297f71d0950b126606224dc8b58b5dde0b9a
Frans Larsen <larsen.frans@gmail.com>
2015-12-21 23:40:57 +0100

    Commented and cleaned a bit up made a const for how long the time should be on

--- led_entrance.ino
+++ led_entrance.ino
@@ -1,31 +1,26 @@
 #include <Time.h>
 
+const int TIME_ON = 4; // How long the timer should be  (In seconds)
+
 void setup() {
-  Serial.begin(9600);
   pinMode(9, INPUT);
   pinMode(8, OUTPUT);
   
   // Lets assume the door is closed
-  // Make LED is turned off
-  // Get time of the day (?)
+  // LED is turned off
+  digitalWrite(8, false);
+  
+  // Stupid ideas:
+  //   Give it a small speaker which can greet the door opener 
 }
 
 void loop() {
-  // digitalWrite(8, digitalRead(9));
-  Serial.println(hour() +":"  + second());
-  // Check time of the day, if between certain timeslot, then proceed
-    // Check if digitalRead(9) outputs 1 (door opened), if yes, proceed
-      // Turn on LED
-      // Delay certain amount of time
-    // Else
-      // Turn off LED
-
-  if(hour() > 19 || hour() < 8) { // If time is between 19:00 and 8:00)
-    if(digitalRead(9) == 0) {
-      digitalWrite(8, true);
-      delay(4000);
-    } else {
-      digitalWrite(8, false);
+  if(hour() > 19 || hour() < 8) {   // If time is between 19:00 and 8:00)
+    if(digitalRead(9) == 0) {       // If the door is opened
+      digitalWrite(8, true);        // Turn on the LED
+      delay(TIME_ON * 1000);        // And wait TIME_ON seconds
+    } else {                        // If door is closed
+      digitalWrite(8, false);       // Turn off the LED
     }
   }    
 }
