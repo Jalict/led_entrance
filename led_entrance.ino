f3a8ec95f10ba09629b328add46fe60fe9d783e6
Frans Larsen <larsen.frans@gmail.com>
2015-12-21 23:41:34 +0100

    Disabled time for now as I need some kind of RTPC

--- led_entrance.ino
+++ led_entrance.ino
@@ -1,4 +1,4 @@
-#include <Time.h>
+//#include <Time.h>
 
 const int TIME_ON = 4; // How long the timer should be  (In seconds)
 
@@ -15,12 +15,12 @@ void setup() {
 }
 
 void loop() {
-  if(hour() > 19 || hour() < 8) {   // If time is between 19:00 and 8:00)
+  //if(hour() > 19 || hour() < 8) {   // If time is between 19:00 and 8:00)
     if(digitalRead(9) == 0) {       // If the door is opened
       digitalWrite(8, true);        // Turn on the LED
       delay(TIME_ON * 1000);        // And wait TIME_ON seconds
     } else {                        // If door is closed
       digitalWrite(8, false);       // Turn off the LED
     }
-  }    
+  //}    
 }
